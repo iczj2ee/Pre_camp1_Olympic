50e9eb9f7c6f2fff3d4f122915cf9a554f919b71
singto <songsingtoy@gmail.com>
2025-09-26 06:35:09 +0700

    C1PR04_Unfinish

--- main.cpp
+++ main.cpp
@@ -1,33 +1,56 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
 using namespace std;
-void convert_to_binary(int n){
-    vector<int> binary_number;
-    while (n > 0){
-        binary_number.push_back(n % 2);
-        n = n / 2;
+
+int convert_to_binary(int n) {
+    if (n == 0) return 0; 
+    int result = 0;
+    int place = 1;
+    while (n > 0) {
+        int bit = n % 2;
+        result = bit * place + result;
+        n /= 2;
+        place *= 10;
     }
-    for (int i = binary_number.size() - 1; i >= 0; i--){
-        cout << binary_number[i];
-    }
-    cout << endl;
+    return result;
 }
 
-
-int main(){
-    int n;
-    
-    cin >> n;
-    int dorayaki[n];
-    int dorayaki_binary_number[n];
-    for(int i = 0; i < n; i++){
-        cin >> dorayaki[i];
+int convert_to_decimal(const string& binary_number) {
+    int decimal_number = 0;
+    for (char c : binary_number) {
+        decimal_number = decimal_number * 2 + (c - '0');
     }
-    
-    
-    for (int i = 0; i < n; i++){
-        convert_to_binary(dorayaki[i]);
+    return decimal_number;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> dorayaki_result(n);
+    char type;
+    for (int i = 0; i < n; i++) {
+        cin >> type;
+        if (type == 'D') {
+            int dorayaki;
+            cin >> dorayaki;
+            dorayaki_result[i] = convert_to_binary(dorayaki);
+        } else if (type == 'B') {
+            string binary_number;
+            cin >> binary_number;
+            dorayaki_result[i] = convert_to_decimal(binary_number);
+        } else {
+            dorayaki_result[i] = -1;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (dorayaki_result[i] == -1) {
+            cout << "Invalid input" << endl;
+        } else {
+            cout << dorayaki_result[i] << endl;
+        }
     }
 
     return 0;
