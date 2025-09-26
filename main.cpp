7f0b9902b9d34823a6d360d47e5c6be19a9a500e
singto <songsingtoy@gmail.com>
2025-09-26 21:32:08 +0700

    C1PR05_Finish

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ int main() {
     for (int i = 0; i < n; i++){
         switch (type_ingredients[i]){
         case 1:
-            for (int line = 0; line <= size_ingredients[i]; line++){
-                for (int star = 0; star < line; star++){
-                    if (star != 0 && star != line - 1 && line != size_ingredients[i]){
+            for (int line = 0; line < size_ingredients[i]; line++){
+                for (int star = 0; star <= line; star++){
+                    if (star != 0 && star != line && line != size_ingredients[i] - 1){
                         cout << "  ";
                     } else {
                         cout << "* ";
@@ -71,6 +71,136 @@ int main() {
                         cout << "* ";
                     }
                     
+                }
+                cout << endl;
+            }
+            break;
+        case 5: 
+            for (int line = 0; line < 2 * size_ingredients[i] - 1; line++){
+                for (int star = 0; star <= line; star++){
+                    if (line < size_ingredients[i]){
+                        if (star != 0 && star != line && line != size_ingredients[i]){
+                            cout << "  ";
+                        } else {
+                            cout << "* ";
+                        }
+                    } else {
+                        int reverse_line = 2 * size_ingredients[i] - line - 1;
+                        if (star != 0 && star != reverse_line - 1 && reverse_line != size_ingredients[i]){
+                            cout << "  ";
+                        } else {
+                            cout << "* ";
+                        }
+                    }
+                }
+                cout << endl;
+            }
+            break;
+        
+        case 6:
+            for (int line = 0; line < size_ingredients[i]; line++){
+                for (int space = 0; space < size_ingredients[i] - (line + 1); space++) {
+                    cout << "  ";
+                }
+                for (int star = 0; star <= 2 * line; star++){
+                    if (star != 0 && star != 2 * line && line != size_ingredients[i] - 1 && line != 0){
+                        cout << "  ";
+                    } else {
+                        cout << "* ";
+                    }
+                }
+                cout << endl;
+            }
+            
+            break;
+        case 7:
+            for (int line = 0; line < 2 * size_ingredients[i] - 1; line++){
+                if (line < size_ingredients[i]){
+                    for (int space = 0; space < size_ingredients[i] - (line + 1); space++) {
+                        cout << "  ";
+
+                    }
+                    for (int star = 0; star <= line; star++){
+                        if (star != 0 && star != line && line != size_ingredients[i]){
+                            cout << "  ";
+                        } 
+                        else {
+                            cout << "* ";
+                        }
+                    }
+                }
+                else {
+                    int reverse_line = 2 * size_ingredients[i] - line - 1;
+                    for (int space = 0; space < (line + 1) - size_ingredients[i]; space++) {
+                        cout << "  ";
+
+                    }
+                    for (int star = 0; star < reverse_line; star++){
+                        if (star != 0 && star != reverse_line - 1 && reverse_line != size_ingredients[i]){
+                            cout << "  ";
+                        } 
+                        else {
+                            cout << "* ";
+                        }
+                    }
+                }
+
+                cout << endl;
+            }
+            break;
+        case 8:
+            for (int line = size_ingredients[i] - 1; line >= 0; line--){
+                for (int space = 0; space < size_ingredients[i] - (line + 1); space++) {
+                    cout << "  ";
+                }
+                for (int star = 0; star <= 2 * line; star++){
+                    if (star != 0 && star != 2 * line && line != size_ingredients[i] - 1 && line != 0){
+                        cout << "  ";
+                    } else {
+                        cout << "* ";
+                    }
+                }
+                cout << endl;
+            }
+            
+            break;
+        case 9:
+            for (int line = 0; line < size_ingredients[i]; line++){
+                for (int star = 0; star < size_ingredients[i]; star++){
+                    if (star != 0 && star !=  size_ingredients[i] - 1 && line != size_ingredients[i] - 1 && line != 0){
+                        cout << "  ";
+                    } 
+                    else {
+                        cout << "* ";
+                    }
+                }
+                cout << endl;
+            }
+            break;
+        case 10:
+            for (int line = 0; line < size_ingredients[i]; line++){
+                for (int space = 0; space < size_ingredients[i] - (line + 1); space++) {
+                    cout << "  ";
+                }
+                for (int star = 0; star <= 2 * line; star++){
+                    if (star != 0 && star != 2 * line && line != 0){
+                        cout << "  ";
+                    } else {
+                        cout << "* ";
+                    }
+                }
+                cout << endl;
+            }
+            for (int line = size_ingredients[i] - 2; line >= 0; line--){
+                for (int space = 0; space < size_ingredients[i] - (line + 1); space++) {
+                    cout << "  ";
+                }
+                for (int star = 0; star <= 2 * line; star++){
+                    if (star != 0 && star != 2 * line && line != 0){
+                        cout << "  ";
+                    } else {
+                        cout << "* ";
+                    }
                 }
                 cout << endl;
             }
