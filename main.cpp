a152373cb98938a9647c3f889701961f610395ff
singto <songsingtoy@gmail.com>
2025-09-26 07:15:25 +0700

    C1PR05_Unfinish_Case-4

--- main.cpp
+++ main.cpp
@@ -1,55 +1,82 @@
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
-
-int convert_to_binary(int n) {
-    if (n == 0) return 0; 
-    int result = 0;
-    int place = 1;
-    while (n > 0) {
-        int bit = n % 2;
-        result = bit * place + result;
-        n /= 2;
-        place *= 10;
-    }
-    return result;
-}
-
-int convert_to_decimal(const string& binary_number) {
-    int decimal_number = 0;
-    for (char c : binary_number) {
-        decimal_number = decimal_number * 2 + (c - '0');
-    }
-    return decimal_number;
-}
 
 int main() {
     int n;
     cin >> n;
+    int type_ingredients[n];
+    int size_ingredients[n];
 
-    vector<int> dorayaki_result(n);
-    char type;
     for (int i = 0; i < n; i++) {
-        cin >> type;
-        if (type == 'D') {
-            int dorayaki;
-            cin >> dorayaki;
-            dorayaki_result[i] = convert_to_binary(dorayaki);
-        } else if (type == 'B') {
-            string binary_number;
-            cin >> binary_number;
-            dorayaki_result[i] = convert_to_decimal(binary_number);
-        } else {
-            dorayaki_result[i] = -1;
-        }
+        cin >> type_ingredients[i] >> size_ingredients[i];
     }
 
-    for (int i = 0; i < n; i++) {
-        if (dorayaki_result[i] == -1) {
-            cout << "Invalid input" << endl;
-        } else {
-            cout << dorayaki_result[i] << endl;
+    for (int i = 0; i < n; i++){
+        switch (type_ingredients[i]){
+        case 1:
+            for (int line = 0; line <= size_ingredients[i]; line++){
+                for (int star = 0; star < line; star++){
+                    if (star != 0 && star != line - 1 && line != size_ingredients[i]){
+                        cout << "  ";
+                    } else {
+                        cout << "* ";
+                    }
+                    
+                }
+                cout << endl;
+            }
+            break;
+        
+        case 2:
+            for (int line = 0; line < size_ingredients[i]; line++){
+                for (int space = 0; space < size_ingredients[i] - (line + 1); space++){
+
+                    cout << "  ";
+                    
+                }
+                for (int star = 0; star <= line; star++){
+                    if (star != 0 && star != line && line != size_ingredients[i] - 1){
+                        cout << "  ";
+                    } else {
+                        cout << "* ";
+                    }
+                }
+                cout << endl;
+            }
+            break;
+        
+        case 3:
+            for (int line = size_ingredients[i] - 1; line >= 0; line--){
+                for (int space = 0; space < size_ingredients[i] - (line + 1); space++){
+
+                    cout << "  ";
+                    
+                }
+                for (int star = 0; star <= line; star++){
+                    if (star != 0 && star != line && line != size_ingredients[i] - 1){
+                        cout << "  ";
+                    } else {
+                        cout << "* ";
+                    }
+                }
+                cout << endl;
+            }
+            break;
+        case 4:
+            for (int line = size_ingredients[i]; line > 0; line--){
+                for (int star = 0; star < line; star++){
+                    if (star != 0 && star != line - 1 && line != size_ingredients[i]){
+                        cout << "  ";
+                    } else {
+                        cout << "* ";
+                    }
+                    
+                }
+                cout << endl;
+            }
+            break;
+        default:
+            break;
         }
     }
 
