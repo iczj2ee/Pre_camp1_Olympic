8bb4918a4581d6c6c5b585132411be4212eea725
Phatthanaphon Sutham <singto1597@gmail.com>
2025-09-30 14:40:24 +0700

    C1PR10_Notest
    Just write the code

--- C1PR10/main.cpp
+++ C1PR10/main.cpp
@@ -2,20 +2,57 @@
 using namespace std;
 
 int main() {
-    string stadium;
-    cin >> stadium;
-    int timeOfVWin = 0;
-    int timeOfVWinMax = 0;
-    for (int i = 0; i < stadium.length(); i++){
-        if (stadium[i] == 'V'){
-            timeOfVWin++;
-        } else{
-            timeOfVWin = 0;
+    string charactorName;
+    int charactorAttack;
+    int characrtorDefense;
+    int charactorHealth;
+
+    int bossAttack;
+    int bossHealth;
+    int bossDefense;
+
+    int damageToBoss;
+    int damageAllToBoss = 0;
+
+    int damageToCharactor;
+
+    cin >> charactorName;
+    cin >> charactorAttack >> characrtorDefense >> charactorHealth;
+    cin >> bossAttack >> bossDefense >> bossHealth;
+
+    while (charactorHealth > 0 && bossHealth > 0){
+        damageToBoss = charactorAttack - bossDefense;
+        if (damageToBoss < 0){
+            damageToBoss = 0;
         }
-        if (timeOfVWin > timeOfVWinMax){
-            timeOfVWinMax = timeOfVWin;
+        bossHealth -= damageToBoss;
+        damageAllToBoss += damageToBoss;
+        if (bossHealth <= 0){
+            break;
         }
+
+        damageToCharactor = bossAttack - characrtorDefense;
+        if (damageToCharactor < 0){
+            damageToCharactor = 0;
+        }
+        charactorHealth -= damageToCharactor;
+        if (charactorHealth <= 0){
+            break;
+        }
+
     }
-    cout << timeOfVWinMax;
+    if (charactorHealth < 0){
+        charactorHealth = 0;
+    }
+    if (bossHealth < 0){
+        bossHealth = 0;
+    }
+    if (charactorHealth > 0 && bossHealth <= 0){
+        cout << "Victory" << endl;
+    }
+    else if (charactorHealth <= 0 && bossHealth > 0){
+        cout << "Defeat" << endl;
+    }
+    cout << charactorName << charactorHealth << damageAllToBoss << endl;
     return 0;
 }
