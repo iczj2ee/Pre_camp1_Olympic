7b8a63972e92f2b1f2d0bf7b2fe0e890b432bc02
Phatthanaphon Sutham <singto1597@gmail.com>
2025-09-30 20:29:06 +0700

    Use formular
    But not Ok

--- C1PR15/main.cpp
+++ C1PR15/main.cpp
@@ -5,22 +5,22 @@ int main() {
     string type;
     int max_size;
     cin >> type >> max_size;
-    int dimension;
     int mod = 1000000007;
-    if (type == "row") dimension = 1;
-    else if (type == "grid") dimension = 2;
-    else if (type == "block") dimension = 3;
-
-
     long long weight = 0;
-
-    for (long long i = 1; i <= max_size; ++i) {
-        long long result = i;
-        if (dimension >= 2) result = (i * i) % mod;   // i^2
-        if (dimension >= 3) result = (i * i * i) % mod;   // i^3
-        weight = (weight + result) % mod;
+    if (type == "row"){
+        weight = (((max_size * (max_size + 1) % mod)) / 2) % mod;
     }
-    cout << ( weight * 24 ) % mod << endl;
+
+    else if (type == "grid"){
+        weight = (((((max_size * max_size) % mod + max_size) % mod) * (((2 * max_size) % mod) + 1)) / 6) % mod;
+    }
+    else if (type == "block"){
+        weight = ((((max_size * (max_size + 1) % mod) / 2) % mod) * (((max_size * (max_size + 1) % mod) / 2) % mod)) % mod;
+    }
+    cout << (weight * 24) % mod << endl;
+    
+
+    
 
     return 0;
 }
