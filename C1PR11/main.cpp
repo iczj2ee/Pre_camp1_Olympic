e4686fcd3e3a30c073fa72d5538040ea0132dcbb
Phatthanaphon Sutham <singto1597@gmail.com>
2025-09-30 15:52:33 +0700

    C1PR11

---
+++ C1PR11/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const long long MOD = 1000000007;
+    long long x = 10;
+    string password;
+    cin >> password;
+
+    for (int i = 0; i < (int)password.length(); i++) {
+        if (password[i] == 'a') {
+            x = (x + 7) % MOD;
+        } 
+        else if (password[i] == 'b') {
+            x = (x * 7) % MOD;
+        } 
+        else if (password[i] == 'c') {
+            x = (x * x) % MOD;
+        }
+    }
+
+    cout << x % MOD << endl;
+    return 0;
+}
