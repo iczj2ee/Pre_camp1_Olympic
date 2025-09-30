250f5c0ce1ea9590cd6d2f5eb41793ae50744ce6
Phatthanaphon Sutham <singto1597@gmail.com>
2025-09-30 16:20:04 +0700

    C1PR13

---
+++ C1PR13/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int value = 0; // This variable is declared but not used
+    for (int i = 1; i <= n; ++i) {
+        if (n % i == 0) {
+            value ++;
+        }
+    }
+    cout << value << endl;
+    return 0;
+}
