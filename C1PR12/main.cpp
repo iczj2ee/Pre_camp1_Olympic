92b91f8d4ff7ef9d3621d11e0621338f0dc59237
Phatthanaphon Sutham <singto1597@gmail.com>
2025-09-30 16:11:45 +0700

    C1PR12

---
+++ C1PR12/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>   // Required for using std::string
+#include <algorithm> // Required for std::reverse()
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string stadium;
+        cin >> stadium;
+        string stadiumReverse = stadium;
+        reverse(stadiumReverse.begin(), stadiumReverse.end());
+        if (stadium == stadiumReverse) {
+            cout << "possible" << endl;
+        } else {
+            cout << "impossible" << endl;
+        }
+    }
+    
+    return 0;
+}
