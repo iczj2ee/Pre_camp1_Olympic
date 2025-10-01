0c1c3f66f21038f8a3e672ca9bdedf0beb2cc96d
Phatthanaphon Sutham <singto1597@gmail.com>
2025-10-01 08:53:01 +0700

    C1PR16

---
+++ C1PR16/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        long long a;
+        cin >> a;
+        int count = 0;
+        for (long long j = 1; j * j <= a; j++) {
+            if (a % j == 0) {
+                count++;
+            }
+            if (count > 1) {
+                break;
+            }
+
+        }
+        if (count <= 1) {
+            cout << "yes" << endl;
+        } else {
+            cout << "no" << endl;
+        }
+    }
+    
+    return 0;
+}
