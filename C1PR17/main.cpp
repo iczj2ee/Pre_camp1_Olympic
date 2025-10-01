4ce9a8f8b4c75a001a466741c3c168a37642e0d4
Phatthanaphon Sutham <singto1597@gmail.com>
2025-10-01 09:05:11 +0700

    C1PR17

---
+++ C1PR17/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+    for (int i = 2; i <= q; i++) {
+        int count = 0;
+        if (i == 2) {
+            cout << i << " ";
+            continue;
+        }
+        if (i % 2 == 0) {
+            continue;
+        }
+        
+        for (long long j = 1; j * j <= i; j++) {
+            if (i % j == 0) {
+                count++;
+            }
+            if (count > 1) {
+                break;
+            }
+
+        }
+        if (count <= 1) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    
+    return 0;
+}
